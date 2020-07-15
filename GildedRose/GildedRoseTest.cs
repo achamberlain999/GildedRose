2be39d2479006e014edc9cb2be66757ee2e44ed5
Alexander Chamberlain <Alexander.Chamberlain@softwire.com>
2020-07-15 12:06:30 +0100

    Test that quality decreases by 1 per day for the dexterity vest

--- GildedRose/GildedRoseTest.cs
+++ GildedRose/GildedRoseTest.cs
@@ -6,12 +6,24 @@ namespace csharpcore
     public class GildedRoseTest
     {
         [Fact]
-        public void foo()
+        public void Foo()
         {
-            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
-            GildedRose app = new GildedRose(Items);
+            IList<Item> items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+            GildedRose app = new GildedRose(items);
             app.UpdateQuality();
-            Assert.Equal("foo", Items[0].Name);
+            Assert.Equal("foo", items[0].Name);
+        }
+
+        [Fact]
+        public void QualityDecreasesWithTime_DexterityVest()
+        {
+            IList<Item> items = new List<Item> { new Item { Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20}};
+            GildedRose app = new GildedRose(items);
+            for (var i = 0; i < 5; i++)
+            {
+                app.UpdateQuality();
+            }
+            Assert.Equal(15, items[0].Quality);
         }
     }
 }
