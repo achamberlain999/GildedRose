f46b7816f68d71577e7208d2ca2500595c8109f7
Alexander Chamberlain <Alexander.Chamberlain@softwire.com>
2020-07-15 16:15:07 +0100

    Removed all for loops in tests

--- GildedRose/GildedRoseTest.cs
+++ GildedRose/GildedRoseTest.cs
@@ -1,67 +1,43 @@
 ﻿using Xunit;
 using System.Collections.Generic;
-using System.Runtime.InteropServices;
 
 namespace csharpcore
 {
     public class GildedRoseTest
     {
         [Fact]
-        public void Foo()
-        {
-            IList<Item> items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
-            GildedRose app = new GildedRose(items);
-            app.UpdateQuality();
-            Assert.Equal("foo", items[0].Name);
-        }
-
-        [Fact]
-        public void QualityDecreasesWithTime_DexterityVest()
+        public void QualityDecreasesWithTime()
         {
             IList<Item> items = new List<Item> { new Item { Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20}};
-            GildedRose app = new GildedRose(items);
-            for (var i = 0; i < 5; i++)
-            {
-                app.UpdateQuality();
-            }
-            Assert.Equal(15, items[0].Quality);
+            var app = new GildedRose(items);
+            app.UpdateQuality();
+            Assert.Equal(19, items[0].Quality);
         }
 
         [Fact]
-        public void SellInDecreasesWithTime_AgedBrie()
+        public void SellInDecreasesWithTime()
         {
-            IList<Item> items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 10, Quality = 20}};
-            GildedRose app = new GildedRose(items);
-            for (var i = 0; i < 8; i++)
-            {
-                app.UpdateQuality();
-            }
-            Assert.Equal(2, items[0].SellIn);
+            IList<Item> items = new List<Item> { new Item { Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20}};
+            var app = new GildedRose(items);
+            app.UpdateQuality();
+            Assert.Equal(9, items[0].SellIn);
         }
         
         [Fact]
-        public void QualityDegradesTwiceAsFastPastSellByDate()
+        public void QualityDecreasesTwiceAsFastPastSellByDate()
         {
-            IList<Item> items = new List<Item> { new Item { Name = "Dexterity Vest", SellIn = 5, Quality = 20}};
-            GildedRose app = new GildedRose(items);
-            for (var i = 0; i < 10; i++)
-            {
-                app.UpdateQuality();
-            }
-
-            Assert.Equal(5, items[0].Quality);
+            IList<Item> items = new List<Item> { new Item { Name = "+5 Dexterity Vest", SellIn = 0, Quality = 20}};
+            var app = new GildedRose(items);
+            app.UpdateQuality();
+            Assert.Equal(18, items[0].Quality);
         }
         
         [Fact]
         public void QualityIsNeverNegative()
         {
-            IList<Item> items = new List<Item> { new Item { Name = "Dexterity Vest", SellIn = 10, Quality = 5}};
-            GildedRose app = new GildedRose(items);
-            for (var i = 0; i < 15; i++)
-            {
-                app.UpdateQuality();
-            }
-
+            IList<Item> items = new List<Item> { new Item { Name = "+5 Dexterity Vest", SellIn = 10, Quality = 0}};
+            var app = new GildedRose(items);
+            app.UpdateQuality();
             Assert.Equal(0, items[0].Quality);
         }
         
@@ -69,26 +45,18 @@ namespace csharpcore
         public void AgedBrieIncreasesInQuality()
         {
             IList<Item> items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 10, Quality = 20}};
-            GildedRose app = new GildedRose(items);
-            for (var i = 0; i < 5; i++)
-            {
-                app.UpdateQuality();
-            }
-
-            Assert.Equal(25, items[0].Quality);
+            var app = new GildedRose(items);
+            app.UpdateQuality();
+            Assert.Equal(21, items[0].Quality);
         }
         
                 
         [Fact]
         public void QualityNeverExceedsFifty()
         {
-            IList<Item> items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 40, Quality = 20}};
-            GildedRose app = new GildedRose(items);
-            for (var i = 0; i < 40; i++)
-            {
-                app.UpdateQuality();
-            }
-
+            IList<Item> items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 20, Quality = 50}};
+            var app = new GildedRose(items);
+            app.UpdateQuality();
             Assert.Equal(50, items[0].Quality);
         }
         
@@ -96,12 +64,8 @@ namespace csharpcore
         public void SulfurasNeedsNotToBeSold()
         {
             IList<Item> items = new List<Item> { new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 10, Quality = 20}};
-            GildedRose app = new GildedRose(items);
-            for (var i = 0; i < 10; i++)
-            {
-                app.UpdateQuality();
-            }
-
+            var app = new GildedRose(items);
+            app.UpdateQuality();
             Assert.Equal(10, items[0].SellIn);
         }
         
@@ -110,63 +74,43 @@ namespace csharpcore
         {
             IList<Item> items = new List<Item> { new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 15, Quality = 20}};
             GildedRose app = new GildedRose(items);
-            for (var i = 0; i < 15; i++)
-            {
-                app.UpdateQuality();
-            }
-
+            app.UpdateQuality();
             Assert.Equal(20, items[0].Quality);
         }
         
         [Fact]
         public void BackstagePassesIncreaseAtRateOneWithMoreThanTenDays()
         {
-            IList<Item> items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 20, Quality = 10}};
-            GildedRose app = new GildedRose(items);
-            for (var i = 0; i < 5; i++)
-            {
-                app.UpdateQuality();
-            }
-
-            Assert.Equal(15, items[0].Quality);
+            IList<Item> items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 20, Quality = 20}};
+            var app = new GildedRose(items);
+            app.UpdateQuality();
+            Assert.Equal(21, items[0].Quality);
         }
         
         [Fact]
         public void BackstagePassesIncreaseAtRateTwoBetweenFiveAndTenDays()
         {
-            IList<Item> items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 10, Quality = 10}};
-            GildedRose app = new GildedRose(items);
-            for (var i = 0; i < 5; i++)
-            {
-                app.UpdateQuality();
-            }
-
-            Assert.Equal(20, items[0].Quality);
+            IList<Item> items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 10, Quality = 20}};
+            var app = new GildedRose(items);
+            app.UpdateQuality();
+            Assert.Equal(22, items[0].Quality);
         }
         
         [Fact]
         public void BackstagePassesIncreaseAtRateThreeBetweenZeroAndFiveDays()
         {
-            IList<Item> items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 5, Quality = 25}};
-            GildedRose app = new GildedRose(items);
-            for (var i = 0; i < 5; i++)
-            {
-                app.UpdateQuality();
-            }
-
-            Assert.Equal(40, items[0].Quality);
+            IList<Item> items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 5, Quality = 20}};
+            var app = new GildedRose(items);
+            app.UpdateQuality();
+            Assert.Equal(23, items[0].Quality);
         }
         
         [Fact]
         public void BackstagePassesQualityIsZeroAfterEvent()
         {
-            IList<Item> items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = -4, Quality = 25}};
-            GildedRose app = new GildedRose(items);
-            for (var i = 0; i < 1; i++)
-            {
-                app.UpdateQuality();
-            }
-
+            IList<Item> items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 0, Quality = 25}};
+            var app = new GildedRose(items);
+            app.UpdateQuality();
             Assert.Equal(0, items[0].Quality);
         }
         
@@ -174,12 +118,8 @@ namespace csharpcore
         public void SulfurasAllowedQualityEighty()
         {
             IList<Item> items = new List<Item> { new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 10, Quality = 80}};
-            GildedRose app = new GildedRose(items);
-            for (var i = 0; i < 1; i++)
-            {
-                app.UpdateQuality();
-            }
-
+            var app = new GildedRose(items);
+            app.UpdateQuality();
             Assert.Equal(80, items[0].Quality);
         }
         
@@ -187,13 +127,9 @@ namespace csharpcore
         public void ConjuredItemsDegradeFaster()
         {
             IList<Item> items = new List<Item> { new Item { Name = "Conjured Mana Cake", SellIn = 10, Quality = 40}};
-            GildedRose app = new GildedRose(items);
-            for (var i = 0; i < 5; i++)
-            {
-                app.UpdateQuality();
-            }
-
-            Assert.Equal(30, items[0].Quality);
+            var app = new GildedRose(items);
+            app.UpdateQuality();
+            Assert.Equal(38, items[0].Quality);
         }
     }
 }
